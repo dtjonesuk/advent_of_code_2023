f1a1867a6e3ad4927e6346c16641ca2b236137e1
David Jones <djonesuk@gmail.com>
2023-12-01 16:31:49 +0000

    Day template

--- main.cpp
+++ main.cpp
@@ -5,50 +5,82 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <functional>
+#include <numeric>
+#include <ranges>
+#include <chrono>
 
 namespace day## {
-/// Change this to the current day
+    /// Change this to the current day
     static const std::string day = "##";
+    static const std::string inputFilename = "..\\day" + day + "\\input.txt";
 
     struct PuzzleInput {
-        PuzzleInput(const std::string &filename) {
+        std::vector<std::string> lines;
+
+        explicit PuzzleInput(const std::string &filename) {
             std::ifstream istream(filename);
             std::string line;
 
             while (std::getline(istream, line)) {
                 // do something
+                lines.push_back(line);
             }
         }
 
     };
 
-    void test(const std::string &filename) {
+    /// Unit Test
+    void test() {
         assert(true);
     }
 
-    int partOne(const std::string &filename) {
-        PuzzleInput input(filename);
-        int result = 0;
+    /// Part One Solution
+    int partOne() {
+        using namespace std::views;
 
-        return result;
+        PuzzleInput input(inputFilename);
+
+        return 0;
     }
 
-    int partTwo(const std::string &filename) {
-        PuzzleInput input(filename);
-        int result = 0;
 
-        return result;
+    /// Part Two Solution
+    int partTwo() {
+        using namespace std::views;
 
+        PuzzleInput input(inputFilename);
+
+        return 0;
+    }
+
+
+    /// Benchmarks a callable
+    template<typename T>
+    std::pair<double, T> eval(std::function<T()> func) {
+        const auto t1 = std::chrono::high_resolution_clock::now();
+        T result = func();
+        const auto t2 = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double> seconds = t2 - t1;
+
+        return std::make_pair(seconds.count(), result);
     }
 
     void run() {
-        const std::string inputFilename = "..\\day" + day + "\\input.txt";
+        test();
 
-        test(inputFilename);
+        {
+            std::cout << std::fixed << std::setprecision(3);
+            auto [seconds, result] = eval<int>(&partOne);
 
-        std::cout << "Day " << day << ": Part One = " << partOne(inputFilename) << std::endl;
+            std::cout << "Day " << day << ": Part One = " << result << "\t (completed in " << seconds << "s).\n";
+        }
+        {
+            std::cout << std::fixed << std::setprecision(3);
+            auto [seconds, result] = eval<int>(&partTwo);
 
-        std::cout << "Day " << day << ": Part Two = " << partTwo(inputFilename) << std::endl;
+            std::cout << "Day " << day << ": Part One = " << result << "\t (completed in " << seconds << "s).\n";
+        }
     }
 }
 
