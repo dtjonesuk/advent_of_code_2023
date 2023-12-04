653b77d66b4f0bfe4b59f251476c0115550ec96d
David Jones <djonesuk@gmail.com>
2023-12-04 22:06:44 +0000

    Day 04 Complete

--- day04/CardCounter.cpp
+++ day04/CardCounter.cpp
@@ -8,7 +8,6 @@ namespace day04 {
     CardCounter::CardCounter(const std::vector<ScratchCard> &cards) : cards(cards) {}
 
     int CardCounter::total() {
-        int total = 0;
         return std::transform_reduce(cards.begin(), cards.end(), 0, std::plus{}, [&](auto& card) {
             return calculate_total_for_card(card);
         });
@@ -17,10 +16,11 @@ namespace day04 {
     int CardCounter::calculate_total_for_card(const ScratchCard &card) {
         int total = 1;
         for (int i = 1; i <= card.winning_count(); ++i) {
-            if ((card.id + i - 1) < cards.size()) {
-                if (!card_totals.contains(card.id + i))
-                    card_totals.emplace(card.id + i, calculate_total_for_card(cards[card.id + i - 1]));
-                total += card_totals[card.id + i];
+            int next_id = card.id + i;
+            if ((next_id - 1) < cards.size()) {
+                if (!card_totals.contains(next_id))
+                    card_totals.emplace(next_id, calculate_total_for_card(cards[next_id - 1]));
+                total += card_totals[next_id];
             }
         }
         return total;
